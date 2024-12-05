5ef8df77b6e590a498e3cdc0a2987bc07fc31017
seanbarlisan <smbarlisan@gmail.com>
2024-12-05 17:51:01 -0500

    O(logn) time complexity

---
+++ binary_search.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1 # sets the variables for left and right to do BST
+
+        while left <= right: # while loop to continue while 0 < right most number index
+            mid = (left + right) // 2 # add the two to get to get middle index
+            if nums[mid] == target: # if the element equals target
+                return mid # return
+            elif mid < target: # if not
+                left = mid + 1 # go through left side and continue
+            else: # otherwise 
+                right = mid - 1 # go through right side and continue
+        return -1
+        
+        
