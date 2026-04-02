02f9d832556c8a7697b51548a2a042dc0769de59
Sean Barlisan <smbarlisan@gmail.com>
2026-04-02 14:00:02 -0400

    Adding the Reverse Linked List problem solution

--- binary_search.py
+++ binary_search.py
@@ -1,6 +1,6 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1 # sets the variables for left and right to do BST
+        left, right = 0, len(nums) - 1 # sets the variables for left and right to do BS
 
         while left <= right: # while loop to continue while 0 < right most number index
             mid = (left + right) // 2 # add the two to get to get middle index
---
+++ reverse_linked_list.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head: # If there is no head, return None because there is nothing to go next
+            return None
+
+        newHead = head # New head is the current head, but we will change it to the last node in the list
+        if head.next:
+            newHead = self.reverseList(head.next) # Recursively call the function again on the self node
+            head.next.next = head # This is a loop, basically says A -> B but B -> A instead, so it's a reversal of the list
+        head.next = None # Then set the current head's next to None, which will be the new tail of the list
+        return newHead
+
+# [1, 2, 3] -> [3, 2, 1] 
