0e407c40679f7995587255593fb9edcd8e99e29e
Sean Barlisan <smbarlisan@gmail.com>
2026-04-09 14:50:37 -0400

    Started solving the Single Number problem

---
+++ singleNumber.py
@@ -0,0 +1,15 @@
+def singleNumber(nums):
+    seen = set()
+    numsRange = range(len(nums))
+    single = 0
+    for x in numsRange:
+        if nums[x] in seen:
+            single = nums[x]
+            seen.add(nums[x])
+        else:
+            single = nums[x]
+    return single
+
+if __name__== "__main__":
+    numsList = [1, 2, 3, 4, 2, 1]
+    singleNumber(numsList)
