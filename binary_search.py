d3c07dd42b3a4cc41826dccb9b8381b1099297a8
Sean Barlisan <smbarlisan@gmail.com>
2026-04-09 14:02:23 -0400

    adjusted the binary search function for lower bound

--- binary_search.py
+++ binary_search.py
@@ -1,15 +1,13 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1 # sets the variables for left and right to do BS
+    def search(self, nums: List[int], target: int) -> int: # lower bound solution to cover everything 
+        l, r = 0, len(nums) # left = 0, right is equal to the length of the nums array
 
-        while left <= right: # while loop to continue while 0 < right most number index
-            mid = (left + right) // 2 # add the two to get to get middle index
-            if nums[mid] == target: # if the element equals target
-                return mid # return
-            elif mid < target: # if not
-                left = mid + 1 # go through right side and continue
-            else: # otherwise 
-                right = mid - 1 # go through left side and continue
-        return -1
+        while l < r: # while left is less than right
+            m = l + ((r - l) // 2) # midpoint calculation
+            if nums[m] > target: # if this is greater than the target
+                r = m # we set right value to the mid point 
+            elif nums[m] <= target:
+                l = m + 1 # decrease lower bound 
+        return l - 1 if (l and nums[l - 1] == target) else -1 # return this function?
         
         
