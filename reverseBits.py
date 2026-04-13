187a2d6c7bf78d1e0e640c1d7fe48d51105a1a4d
Sean Barlisan <smbarlisan@gmail.com>
2026-04-13 14:36:50 -0400

    this is the solution given, going to break it down

--- reverseBits.py
+++ reverseBits.py
@@ -1,6 +1,10 @@
 def reverseBits(bits):
-    
-    return 0 
+    res = 0
+    for i in range(32):
+        bit = (n >> 1) & 1
+        res += (bit << (31 - i))
+
+    return res
 
 if __name__ == "__main__":
 
