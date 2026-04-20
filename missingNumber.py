1b7659238aa792c82160aceff0d1f6a052ee30de
Sean Barlisan <smbarlisan@gmail.com>
2026-04-20 14:53:58 -0400

    solved using Hash Set

--- missingNumber.py
+++ missingNumber.py
@@ -1,15 +1,11 @@
 def missingNumber(nums):
-        missing = 0
-        counter = 0
-        for x in nums:
-            if x == counter: # something is wrong
-                print(nums[x])
-                x += 1
-            else:
-                missing = x
-        return missing
+    num_set = set(nums)
+    n = len(nums)
+    for i in range(n+1):
+        if i not in num_set:
+            return i
 
 if __name__ == "__main__":
-    nums = [1, 2, 3]
+    nums = [0, 1, 3]
     x = missingNumber(nums)
     print(x)
