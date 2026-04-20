1c4b8b551d9e52b23ebb365742fa5dfda2bfe06c
Sean Barlisan <smbarlisan@gmail.com>
2026-04-20 14:54:36 -0400

    included my thought process and notes

--- missingNumber.py
+++ missingNumber.py
@@ -1,9 +1,9 @@
 def missingNumber(nums):
-    num_set = set(nums)
-    n = len(nums)
-    for i in range(n+1):
-        if i not in num_set:
-            return i
+    num_set = set(nums) # create a set of the nums values
+    n = len(nums) # length of the nums list
+    for i in range(n+1): # for range of n
+        if i not in num_set: # if value is not in that set
+            return i # return it
 
 if __name__ == "__main__":
     nums = [0, 1, 3]
